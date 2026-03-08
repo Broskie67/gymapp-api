b171d741f8b307a8a9ab06493ca079971ac3f7a9
Broskie67 <127433699+Broskie67@users.noreply.github.com>
2026-03-08 15:35:31 -0400

    add authentication to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express, { Application, Request, Response } from 'express'
 import { env } from './env'
 import { getPool } from './db'
+import authRoutes from './auth/auth.routes'
 
 const app: Application = express()
+
+app.use(express.json())
 
 app.get('/', (_req: Request, res: Response) => {
   res.send('Hello World with TypeScript and Express!')
@@ -14,6 +17,8 @@ app.get('/health/db', async (_req: Request, res: Response) => {
   console.log("Succes")
   res.json({ db: result.recordset[0] })
 })
+
+app.use('/auth', authRoutes)
 
 app.listen(env.PORT, env.HOST, () => {
   console.log(`Server running at http://${env.HOST}:${env.PORT}`)
